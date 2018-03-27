701c8946788d2eb13de9925ac0a174e3d9cd0372
Matías Acosta <matiasacosta1995@gmail.com>
2018-03-27 19:09:40 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# fixture-rusia-2018
---
+++ static/js/barra.js
@@ -0,0 +1,19 @@
+// Menu-toggle button
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+            $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+            $('nav').addClass('black');
+    }
+
+    else {
+            $('nav').removeClass('black');
+    }
+})
