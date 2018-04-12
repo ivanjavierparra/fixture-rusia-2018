983fd0130e922c84ee5eb2356a566f882277c884
ivanjavierparra <ivanjavierparra@hotmail.com>
2018-04-12 13:32:22 -0300

    empezando aceptarModal

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,16 @@
 var app = (function(scope = {}) {
     let fixture = {};
+	let modal_activo = "";
 
     var equipoFactory = (equipo) => {
         return Object.assign({},{nombre: equipo.nombre})
     }
  
     var partidoFactory = (partido) => {
-        return Object.assign({},partido,{goles_local:0, goles_visitante:0})
+        return Object.assign({},partido,{goles_local:0, goles_visitante:0,
+        setGolesLocal: function(goles) {this.goles_local = goles},
+        setGolesVisitante: function(x) {this.goles_visitante = x}
+        })
     }
 
     var grupoFactory = (grupo) => {
@@ -21,7 +25,7 @@ var app = (function(scope = {}) {
 
         return Object.assign({},{
             letra:grupo.letra,
-            poscion1:grupo.posicion1,
+            posicion1:grupo.posicion1,
             posicion2:grupo.posicion2,
             equipos: equiposACargar,
             partidos: partidosACargar,
@@ -33,8 +37,8 @@ var app = (function(scope = {}) {
 
     var fixtureFactory = (datos) => {
         var gruposACargar = [];
-        for(var i = 0; i < datos.grupos.length; i++){
-            gruposACargar[i] = grupoFactory(datos.grupos[i])
+        for(var i = 0; i < datos.grupos.length; i++){ // probar con datos.length
+            gruposACargar[i] = grupoFactory(datos.grupos[i]) // probar con datos[i]
         }
         return Object.assign({},{grupos: gruposACargar,
         getGrupo: function(letra){
@@ -54,6 +58,7 @@ var app = (function(scope = {}) {
     var getFixture = () => {return fixture} 
 
     var cargarModal = (letra) => {
+		modal_activo = letra;
         var grupo = fixture.getGrupo(letra);
         for (var i=0;i<grupo.partidos.length;i++){
             var partido = grupo.partidos[i];
@@ -75,7 +80,34 @@ var app = (function(scope = {}) {
         console.log(grupo);
 
     }
+	
+	
+	var aceptarModal = () => {
+        console.log("El modal activo es: " + modal_activo);
+        var grupo = fixture.getGrupo(modal_activo);
+        console.log(grupo.partidos[0]);
+        var inputs = $(".input_modal");
+        var contador = 0;
 
-    return Object.assign(scope, {getFixture,run,grupoFactory,fixtureFactory,cargarModal});
+        //recorro inputs y seteo partidos
+        for (var i=0;i<grupo.partidos.length;i++){
+            grupo.partidos[i].setGolesLocal ( inputs[contador].value );
+            contador++;
+            grupo.partidos[i].setGolesVisitante ( inputs[contador].value );
+        }
+
+        
+        //calculo ganador
+
+
+
+        //armo tabla con puntos y goles
+
+
+
+        //paso datos a llaves-phone y llaves-desktop
+    }
+
+    return Object.assign(scope, {getFixture,run,grupoFactory,fixtureFactory,cargarModal,aceptarModal});
 
 })();
